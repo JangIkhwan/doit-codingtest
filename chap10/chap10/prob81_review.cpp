e6fc7728db48c6b310e0cc034e8e72d956592807
JangIkhwan <bluensky0213@gmail.com>
2023-08-09 22:04:31 +0900

    230809 문제 해결 내용

---
+++ chap10/chap10/prob81_review.cpp
@@ -0,0 +1,90 @@
+﻿/*
+23-08-08 15:22 ~ 
+
+맨 앞부터 숫자를 차례로 선택함
+K를 이용해서 선택되지 않은 숫자 중 몇번째 수를 선택해야 하는지 알아내야 함.
+i번째 수를 선택하고, K -= (i - 1) * P[remain]을 수행함.
+
+주어진 수열의 맨 앞부터 차례로 판단함
+만약 선택되지 않은 수에서 i번째 수라면 K += (i - 1) * P[remain]을 수행함.
+
+기억할 점
+- 입력 값의 범위를 꼭 확인하자
+- 팩토리얼이나 조합을 사용할 때 오버플로우가 발생할 수 있으니 주의하자.
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+int seq[21];
+bool picked[21];
+long long P[21];
+
+void printSequence(int N, long long K) {
+	int toPick;
+	for (int now = 0; now < N; now++) {
+		int i = 0;
+		while (K > i * P[N - now - 1]) {
+			i++;
+		}
+		K -= (i - 1) * P[N - now - 1];
+		int j = 0;
+		for (toPick = 1; toPick <= N; toPick++) {
+			if (!picked[toPick]) {
+				if (i - 1 == j) {
+					break;
+				}
+				j++;
+			}
+		}
+		picked[toPick] = true;
+		seq[now] = toPick;
+	}
+	for (int i = 0; i < N; i++) {
+		printf("%d ", seq[i]);
+	}
+}
+
+void printNumber(int N) {
+	long long k = 0; //
+	int i;
+	for (int now = 0; now < N - 1; now++) {
+		i = 0;
+		for (int toPick = 1; toPick <= N; toPick++) {
+			if (!picked[toPick]){
+				if (seq[now] == toPick) {
+					k += i * P[N - now - 1];
+					picked[toPick] = true; // 
+					break;
+				}
+				i++;
+			}
+		}
+	}
+	k += 1;
+	printf("%lld", k);
+};
+
+int main(void) {
+	int N;
+	long long K;
+	int prob;
+	scanf("%d", &N);
+	P[0] = 1;
+	for (int i = 1; i <= N; i++) {
+		P[i] = i * P[i - 1];
+	}
+	scanf("%d", &prob);
+	if (prob == 1) {
+		scanf("%lld", &K); //
+		printSequence(N, K);
+	}
+	else if (prob == 2) {
+		for (int i = 0; i < N; i++) {
+			scanf("%d", &seq[i]);
+		}
+		printNumber(N);
+	}
+	return 0;
+}
+
---
+++ chap10/chap10/prob82.cpp
@@ -0,0 +1,92 @@
+﻿/*
+https://www.acmicpc.net/problem/1256
+
+23-08-08 20:27 ~ 21:37 
+
+aazz 1
+azaz 2
+azza 3
+zaaz 4
+zaza 5
+zzaa 6
+
+남은 a의 수가 a라고 가정
+K가 C[i - 1][z]보다 크면 i번째 문자는 z, 그렇지 않으면 a.
+
+
+aaazz 1
+aazaz 2
+aazza 3
+azaaz 4
+azaza 5
+
+기억할 점
+- 입력값의 범위를 잘 확인해서 조합을 어디까지 계산해야하는지 생각해보자
+- 조합에서는 오버플로우가 발생할 수 있음
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int MAX = 1000000000; //
+long long C[201][101];
+
+void printString(int N, int M, int K) {
+	char result[201];
+	int total = N + M;
+	int aCount = 0;
+	int zCount = 0;
+	for (int i = 0; i <= total; i++) {
+		for(int j = 0; j <= min(i, M); j++) {
+			if (i == j || j == 0) {
+				C[i][j] = 1;
+			}
+			else {
+				C[i][j] = C[i - 1][j] + C[i - 1][j - 1];
+				if (C[i][j] > MAX || C[i][j] < 0) C[i][j] = MAX + 1; // 오버플로우 방지 위해 추가
+			}
+		}
+	}
+	/*for (int i = 0; i <= total; i++) {
+		for (int j = 0; j <= min(i, M); j++) {
+			printf("%lld ", C[i][j]);
+		}
+		printf("\n");
+	}*/
+	
+	if (K > C[total][M]) { // 오버플로우를 방지하지 않으면 -1이 출력이 안됨.
+		printf("-1\n");
+		return;
+	}
+	for (int now = 0; now < total; now++) {
+		if (K > C[total - now - 1][M - zCount]) {
+			result[now] = 'z';
+			K -= C[total - now - 1][M - zCount];
+			zCount += 1; // 
+		}
+		else {
+			if (aCount < N) {
+				result[now] = 'a';
+				aCount += 1;
+				continue;
+			}
+			if(zCount < M){
+				result[now] = 'z';
+				zCount += 1;
+			}
+		}
+	}
+	result[total] = '\0';
+	printf("%s\n", result);
+}
+
+int main(void) {
+	int N, M, K;
+	scanf("%d %d %d", &N, &M, &K);
+	
+	printString(N, M, K);
+	return 0;
+}
+
